cbbe816d4a719c91296be67da0bd241656900e61
Karl Rixon <karlrixon@gmail.com>
2018-08-08 22:25:47 +0100

    Add homepage and basic styling

--- src/main/java/com/krixon/paneladminui/PanelAdminUiApplication.java
+++ src/main/java/com/krixon/paneladminui/PanelAdminUiApplication.java
@@ -2,11 +2,46 @@ package com.krixon.paneladminui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class PanelAdminUiApplication {
+@Configuration
+public class PanelAdminUiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PanelAdminUiApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+
+        resolver.setDefaultLocale(Locale.ENGLISH);
+
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+
+        interceptor.setParamName("language");
+
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
