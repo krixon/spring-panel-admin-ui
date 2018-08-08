018b445b41f41c0ec9cdf0dacb4c5471ba62793b
Karl Rixon <karlrixon@gmail.com>
2018-08-08 18:08:41 +0100

    Initial commit

---
+++ src/main/java/com/krixon/paneladminui/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.krixon.paneladminui;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${bgcolor}")
+    String backgroundColor = "white";
+
+
+    @RequestMapping("/config-client/demo")
+    public String demo(Model model) {
+
+        model.addAttribute("backgroundColor", backgroundColor);
+
+        return "config-client/demo";
+    }
+}
---
+++ src/main/java/com/krixon/paneladminui/PanelAdminUiApplication.java
@@ -0,0 +1,12 @@
+package com.krixon.paneladminui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelAdminUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanelAdminUiApplication.class, args);
+    }
+}
